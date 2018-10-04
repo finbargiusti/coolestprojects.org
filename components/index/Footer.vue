ed807531a10a33356b7ec885c87c51e55ffc0f7d
Daniel Brierton <daniel@brierton.me>
2018-10-04 11:27:12 +0100

    Update news link in footer

--- components/index/Footer.vue
+++ components/index/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="c-footer">
     <div class="c-footer__content">
       <div class="c-footer__links">
-        <a href="https://coolestprojects.org/category/news/">News</a>
+        <a href="https://coolestprojects.org/news/">News</a>
         <a href="https://help.coolestprojects.org/hc">Help centre</a>
         <a href="mailto:hello@coolestprojects.org">Contact us</a>
         <div class="c-footer__social">
